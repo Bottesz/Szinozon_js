0685592604f18de5545748954101ef0c7e866c1a
S4mur419020 <muraitomi04@gmail.com>
2025-05-29 13:16:12 +0200

    jatekter

--- Jatek.js
+++ Jatek.js
@@ -1,67 +1,43 @@
-import { szinekLista, generaltLista, tippLista, visszajelzesLista } from "./listak.js";
+import JatekSor from "./JatekSor.js";
 import Generalt from "./Generalt.js";
-import JatekSor from "./Jateksor.js";
+import { szinekLista, generaltLista, visszajelzesListaM, tippListaM } from "./listak.js";
+
 
 export default class Jatek {
-    #szinekLista=[];
-    #generaltLista=[];
-    #visszajelzesListaM=[];
-    #tippLista=[];
-    #jatekter;
-    #sor;
-    #oszlop;
-
-    constructor() {
-        this.#szinekLista = szinekLista;
-        this.#generaltLista = generaltLista;
-        this.#visszajelzesListaM = visszajelzesLista;
-        this.#tippLista = tippLista;
-        this.#jatekter = document.querySelector(".jatekter");
-        this.#sor = 0;
-        this.#oszlop = 0;
-        this.#init();
+  #szinekLista = [];
+  #generaltLista = [];
+  #visszajelzesListaM = [];
+  #tippListaM = [];
+  #jatekter;
+  #sor = 0;
+  #oszlop = 0;
+  #szin1Elem;
+  #szin2Elem;
+  #szin3Elem;
+  #szin4Elem;
+  #szin5Elem;
+  #szin6Elem;
+  constructor() {
+    this.#jatekter = document.querySelector(".jatekter");
+    this.#szin1Elem = document.querySelector(".szin:nth-child(1)");
+    this.#szin2Elem = document.querySelector(".szin:nth-child(2)");
+    this.#szin3Elem = document.querySelector(".szin:nth-child(3)");
+    this.#szin4Elem = document.querySelector(".szin:nth-child(4)");
+    this.#szin5Elem = document.querySelector(".szin:nth-child(5)");
+    this.#szin6Elem = document.querySelector(".szin:nth-child(6)");
+    this.megjelenit()
+  }
+  megjelenit() {
+    for (let index = 0; index < 10; index++) {
+        new JatekSor(this.#visszajelzesListaM[index], this.#tippListaM[index], this.#jatekter)
     }
-    megjelenit(){
-        const generalt = new Generalt(this.#generaltLista);
-        this.#jatekter.innerHTML = generalt.megjelenit();
-    }
-
-
-    generaltListaEloallit(){
-
-    }
-
-
-    szin1Esemenykezelo(){
-
-    }
-
-    szin2Esemenykezelo(){
-        
-    }
-
-    szin3Esemenykezelo(){
-
-    }
-
-    szin4Esemenykezelo(){
-
-    }
-
-    szin5Esemenykezelo(){
-
-    }
-
-    szin6Esemenykezelo(){
-
-    }
-
-    #init(){
-
-
-    }
-
-    visszajelzes(){
-        
-    }
+  }
+  generaltListaEloallit() {}
+  szin1Esemenykezelo() {}
+  szin2Esemenykezelo() {}
+  szin3Esemenykezelo() {}
+  szin4Esemenykezelo() {}
+  szin5Esemenykezelo() {}
+  szin6Esemenykezelo() {}
+  visszajelzes() {}
 }
