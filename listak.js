3a781bcde5662b34624a33ee326134e9c4f9fcaf
S4mur419020 <muraitomi04@gmail.com>
2025-05-29 10:23:50 +0200

    lista egeszites

--- listak.js
+++ listak.js
@@ -1,6 +1,29 @@
-export const tippLista=["white","white","white","white"]
 export const generaltLista=["white","white","white","white"]
-export const visszajelzesLista=["gray","gray","gray","gray"]
 export const szinekLista=["red","blue","green","yellow","orange","purple"]
+export const tippLista=[
+    ["white","white","white","white"]
+    ["white","white","white","white"]
+    ["white","white","white","white"]
+    ["white","white","white","white"]
+    ["white","white","white","white"]
+    ["white","white","white","white"]
+    ["white","white","white","white"]
+    ["white","white","white","white"]
+    ["white","white","white","white"]
+    ["white","white","white","white"]
+]
+export const visszajelzesLista=[
+    ["gray","gray","gray","gray"]
+    ["gray","gray","gray","gray"]
+    ["gray","gray","gray","gray"]
+    ["gray","gray","gray","gray"]
+    ["gray","gray","gray","gray"]
+    ["gray","gray","gray","gray"]
+    ["gray","gray","gray","gray"]
+    ["gray","gray","gray","gray"]
+    ["gray","gray","gray","gray"]
+    ["gray","gray","gray","gray"]
+]
 
 
+/*tippLista[0][1]*/
