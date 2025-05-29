23c8c3542dc1ef2990de637cbdde0fab2f6d3058
S4mur419020 <muraitomi04@gmail.com>
2025-05-29 09:45:34 +0200

    listak

--- listak.js
+++ listak.js
@@ -1,1 +1,10 @@
-export 
+export default class Listak{
+    tippLista=["white","white","white","white"];
+
+    generaltLista=["white","white","white","white"];
+
+    visszajelzesLista=["gray","gray","gray","gray"];
+
+    szinekLista=["red","blue","green","yellow","orange","purple"];
+}
+
